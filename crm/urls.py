64002ab9dd192ef12acba750556ac7edfce9865f
Tomek <kontakt@cyberhelp.pl>
2025-02-22 20:24:11 +0100

    Zadanie 5 i 6 - stworzono widok z listą szpitali

---
+++ crm/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from . import views
+
+app_name = 'crm'
+
+urlpatterns = [
+    path('hospitals', views.HospitalListView.as_view(), name='hospitals'),
+]
--- crm/views.py
+++ crm/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Hospital
 
-# Create your views here.
+
+class HospitalListView(ListView):
+    model = Hospital
+    template_name = 'crm/hospitals.html'
+    context_object_name = 'hospitals'
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        for obj in queryset:
+            print(obj)  # Wyświetlanie instancji modelu w konsoli
+        return queryset
+
+
